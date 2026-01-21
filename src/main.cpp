470c2ecd92fd0a3bc12502eadc2f8ee5fd2d7cc3
Evan <mrevansolid@gmail.com>
2026-01-21 16:29:05 -0600

    Finished chapter Textures through the disco box section

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,12 @@
 
 #include "shader/shader.h"
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "images/stb_image.h"
+
 #include <iostream>
 #include <cmath>
+#include <filesystem>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
@@ -36,53 +40,65 @@ int main() {
         return -1;
     }
 
-
     // build and compile the shader program
     Shader ourShader("include/shader/shader.vs", "include/shader/shader.fs");
 
     // set up the vertex data and buffers and configure vertex attributes
-    float firstTriangle[] = {
-        // positions       colors
-        0.1f,  0.1f, 0.0f, 1.0f, 0.0f, 0.0f, 
-        0.9f, -0.7f, 0.0f, 0.0f, 0.0f, 1.0f,  
-        0.1f, -0.7f, 0.0f, 0.0f, 1.0f, 0.0f
-    };
-    float secondTriangle[] = {
-        0.1f,  0.1f, 0.0f, 1.0f, 0.0f, 0.0f,
-        0.9f,  0.1f, 0.0f, 0.0f, 1.0f, 0.0f,
-        0.9f, -0.7f, 0.0f, 0.0f, 0.0f, 1.0f
-    };
 
-    unsigned int VBOs[2], VAOs[2];
-    glGenVertexArrays(2, VAOs);
-    glGenBuffers(2, VBOs);
+    float vertices[] = {
+        // positions          // colors            // texture coords
+         0.75f,  0.75f, 0.0f,    1.0f, 0.0f, 0.0f,    1.0f, 1.0f, // top right
+         0.5f, -0.5f, 0.0f,    0.0f, 1.0f, 0.0f,    1.0f, 0.0f, // bottom right
+        -0.5f, -0.5f, 0.0f,    0.0f, 0.0f, 1.0f,    0.0f, 0.0f, // bottom left
+        -0.5f,  0.5f, 0.0f,    1.0f, 1.0f, 0.0f,    0.0f, 1.0f  // top left
+    };
+    unsigned int indices[] {
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
+    };
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
 
-    // first triangle setup
-    glBindVertexArray(VAOs[0]);
-    glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(firstTriangle), firstTriangle, GL_STATIC_DRAW);
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
     //color attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
+    // texture coord attribute
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
 
-    // second triangle setup
-    glBindVertexArray(VAOs[1]);
-    glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(secondTriangle), secondTriangle, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-
-    // this unbinds the VAO so other calls don't accidentally modify it, but this is apparently rare so it is not always necessary.
-    glBindVertexArray(0);
-
-    // comment or uncomment this line for wireframe polygons.
-    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
+    // loading and creating a texture;
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture); // all of the next operations on GL_TEXTURE_2D now have effect on this texture
+    // set the texture wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // set the texture filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // load image, create texture and generate mipmaps
+    int width, height, nrChannels;
+    unsigned char *data = stbi_load("C:/Users/mreva/Code/logl/include/images/flower_bee.jpg", &width, &height, &nrChannels, 0);
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
 
     // rendering loop while the window is open
     while(!glfwWindowShouldClose(window)) {
@@ -93,15 +109,13 @@ int main() {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // for the first triangle, use first shader
+        // bind the texture
+        glBindTexture(GL_TEXTURE_2D, texture);
+
+        // render the rectangle with the image
         ourShader.use();
-
-        // render the triangles
-        glBindVertexArray(VAOs[0]);
-        glDrawArrays(GL_TRIANGLES,0, 3);
-
-        glBindVertexArray(VAOs[1]);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         // glfw: swap the buffers and poll IO events (key presses and more)
         glfwSwapBuffers(window);
@@ -109,8 +123,9 @@ int main() {
     }
 
     // de-allocate resources once they've outlived their purpose
-    glDeleteVertexArrays(2, VAOs);
-    glDeleteBuffers(2, VBOs);
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     
     // glfw: terminate, clearing all previously allocated GLFW resources
     glfwTerminate();
